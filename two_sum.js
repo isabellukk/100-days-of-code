f1f10e93f530b59b4d2f6a6975213ce0948ca8c8
Isabellukk <isabellukk@gmail.com>
2023-05-01 19:09:08 -0700

    two_sum.js

---
+++ two_sum.js
@@ -0,0 +1,18 @@
+// ----- Two Sum ----- //
+
+// Difficulty: Easy
+// Link: https://leetcode.com/problems/two-sum/
+
+var twoSum = function (nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    let second = nums.indexOf(target - nums[i], i + 1);
+    if (second > i) {
+      return [i, second];
+    }
+  }
+};
+
+let nums = [2, 7, 11, 15];
+let target = 9;
+
+console.log(twoSum(nums, target));
