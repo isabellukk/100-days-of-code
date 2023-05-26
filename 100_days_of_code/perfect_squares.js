4e8b586940761c7bac26f3290c3362dd6625981c
Isabellukk <isabellukk@gmail.com>
2023-05-25 22:06:39 -0700

    completed problem

---
+++ 100_days_of_code/perfect_squares.js
@@ -0,0 +1,16 @@
+var numSquares = function (n) {
+  while (n % 4 == 0) {
+    n /= 4;
+  }
+  if (n % 8 == 7) return 4;
+  for (let a = 0; a * a <= n; a++) {
+    let b = Math.floor(Math.sqrt(n - a * a));
+    if (a * a + b * b == n) return !!a + !!b;
+  }
+  return 3;
+};
+
+let n = 12;
+// Expected Output: 3
+
+console.log(numSquares(n));
