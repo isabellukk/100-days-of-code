e93730df0107128a4abd96f2aa0174cfb1e54261
Isabellukk <isabellukk@gmail.com>
2023-06-08 04:02:38 -0700

    completed problem

---
+++ 100_days_of_code/word_pattern.js
@@ -0,0 +1,32 @@
+// ----- Word Pattern----- //
+
+// Difficulty: Easy
+// Link: https://leetcode.com/problems/word-pattern/description/
+
+var wordPattern = function (pattern, s) {
+  const arr = s.split(' '),
+    temp = {};
+
+  if (
+    arr.length !== pattern.length ||
+    new Set([...pattern]).size !== new Set(arr).size
+  )
+    return false;
+
+  for (let i = 0; i < pattern.length; i++) {
+    // if theres nothing in temp, set it equal to the pattern
+    if (!temp[pattern[i]]) {
+      temp[pattern[i]] = arr[i];
+      console.log(temp[pattern[i]]);
+    } else if (temp[pattern[i]] !== arr[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+let pattern = 'abba';
+let s = 'dog cat cat dog';
+// Expected Output: true;
+
+console.log(wordPattern(pattern, s));
