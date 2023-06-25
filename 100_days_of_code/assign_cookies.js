b37cbe006c0e6c3528c7c6483468cbdc0f6cdd91
Isabellukk <isabellukk@gmail.com>
2023-06-25 06:12:34 -0700

    completed problem

---
+++ 100_days_of_code/assign_cookies.js
@@ -0,0 +1,21 @@
+// ----- String Compression ---- //
+
+// Difficulty: Medium
+// Link: https://leetcode.com/problems/string-compression/description/
+
+var findContentChildren = function (g, s) {
+  g.sort((a, b) => a - b);
+  s.sort((a, b) => a - b);
+  let j = 0,
+    res = 0;
+  for (let num of s) {
+    if (num >= g[j]) res++, j++;
+  }
+  return res;
+};
+
+let g = [1, 2, 3];
+let s = [1, 1];
+// Expected Output: 1
+
+console.log(findContentChildren(g, s));
