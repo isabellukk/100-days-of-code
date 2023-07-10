e94a2168f69af8efa4f0950c489589a4c378308d
Isabellukk <isabellukk@gmail.com>
2023-07-10 07:18:59 -0700

    completed problem

---
+++ 100_days_of_code/sleep.js
@@ -0,0 +1,11 @@
+// ----- Sleep ---- //
+
+// Difficulty: Easy
+// Link: https://leetcode.com/problems/sleep/description/
+
+async function sleep(millis) {
+  await new Promise((resolve) => setTimeout(resolve, millis));
+}
+
+let t = Date.now();
+sleep(100).then(() => console.log(Date.now() - t)); // 100
